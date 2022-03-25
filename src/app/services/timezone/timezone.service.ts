ab1b1e5aa7bd1f6a849d96ec1a284c504b2667dc
SergioSerranoP <sergio.serrano@argoecosystem.com>
2022-03-25 13:43:08 +0100

    Added map to tmz service to prevent multiple calls to same endpoint

--- src/app/services/timezone/timezone.service.ts
+++ src/app/services/timezone/timezone.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map, Observable, shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TimezoneService {
+  private apiUrl = 'http://worldtimeapi.org/api/timezone/';
+  private tmzs: Map<string, Observable<object>> = new Map<string, Observable<object>>();
 
   constructor(
     protected http: HttpClient
   ) { }
 
   getTime(area: string, location: string): any {
-    const url = 'http://worldtimeapi.org/api/timezone/' + (area ? (area + '/') : '') + (location || '');
-    return this.http.get(url);
+    const url = this.apiUrl + (area ? (area + '/') : '') + (location || '');
+
+    // Map for preventing calling multiple times the same url
+    if (!this.tmzs.has(url)) {
+      this.tmzs.set(
+        url,
+        this.http.get(url).pipe(
+          map((response: Response) => {
+            return response;
+          }),
+          shareReplay(1)
+        )
+      );
+    }
+    return this.tmzs.get(url);
   }
 }
